8b805aa753ba9395e2cdd13aa97e948c29a7816b
Pham Van Kieu <phamvankieu92@gmail.com>
2023-10-24 10:25:45 +0700

    State

--- src/views/Examples/MyComponent.js
+++ src/views/Examples/MyComponent.js
@@ -1,9 +1,43 @@
 import React from "react";
 
 class MyComponent extends React.Component {
+    state = {
+        name: 'Kiểu Phạm',
+        company: 'KDE'
+    }
+
+    handleOnChangeName = (event) => {
+        this.setState({
+            name: event.target.value
+        });
+    }
+
     render() {
+        let name = 'Kiểu'
         return (
-            <div> Hello my Component KDE</div>
+            // <React.Fragment>
+            <>
+                <div className="name"> 
+                    {console.log('My name is ' + name)}
+                    Hello my Component. My name is {name}
+                </div>
+                <div className="name2"> 
+                    {console.log('My name is ' + name)}
+                    Bạn có muốn tôi giúp gì không?
+                </div>
+
+                <div className="stagename">
+                    <input type="text" value={this.state.name} onChange={(event) => this.handleOnChangeName(event)} />
+                    Tôi tên là {this.state.name}
+                </div>
+                <div className="company">
+                    Tôi làm việc tại {this.state['company']}
+                </div>
+
+            </>
+                
+            // </React.Fragment>
+            
         )
     }
 }
