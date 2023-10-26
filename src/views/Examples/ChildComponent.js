91a4c0e708e022924f1cd8f00d4f0d4d5201fcf6
Pham Van Kieu <phamvankieu92@gmail.com>
2023-10-26 14:31:40 +0700

    Stateless/Statefull Components

--- src/views/Examples/ChildComponent.js
+++ src/views/Examples/ChildComponent.js
@@ -1,36 +1,59 @@
 import React from "react";
 
-class ChildComponent extends React.Component {
+// class ChildComponent extends React.Component {
     
-    render() {
-        console.log('>>>Check data input: ', this.props)
-        // let name = this.props.name;
-        // let age = this.props.age;
+//     render() {
+//         console.log('>>>Check data input: ', this.props)
+//         // let name = this.props.name;
+//         // let age = this.props.age;
 
-        // let {name, age, arrJobs} = this.props;
+//         // let {name, age, arrJobs} = this.props;
         
-        let arrJobs = this.props.arrJobs;
+//         let arrJobs = this.props.arrJobs;
         
-        return (
-            // <React.Fragment>
+//         return (
+//             // <React.Fragment>
             
-            <>
-                <div className="job-lists">
-                    {
-                        arrJobs.map((item,index) => {
-                            return (
-                                <div key={item.id}>
-                                    ID: {item.id} - Title: {item.title} - Salary: {item.salary}
-                                </div>
-                            )
-                        })
-                    }
-                </div>
+//             <>
+//                 <div className="job-lists">
+//                     {
+//                         arrJobs.map((item,index) => {
+//                             return (
+//                                 <div key={item.id}>
+//                                     ID: {item.id} - Title: {item.title} - Salary: {item.salary}
+//                                 </div>
+//                             )
+//                         })
+//                     }
+//                 </div>
                 
-            </>
-            // </React.Fragment>
-        )
-    }
+//             </>
+//             // </React.Fragment>
+//         )
+//     }
+// }
+
+const ChildComponent = (props)=>{
+    let arrJobs = props.arrJobs;
+        
+    return (
+        // <React.Fragment>
+        <>
+            <div className="job-lists">
+                {
+                    arrJobs.map((item,index) => {
+                        return (
+                            <div key={item.id}>
+                                ID: {item.id} - Title: {item.title} - Salary: {item.salary}
+                            </div>
+                        )
+                    })
+                }
+            </div>
+            
+        </>
+        // </React.Fragment>
+    )
 }
- 
+
 export default ChildComponent
---
+++ src/views/Examples/ChildComponent2.js
@@ -0,0 +1,36 @@
+import React from "react";
+
+class ChildComponent extends React.Component {
+    
+    render() {
+        console.log('>>>Check data input: ', this.props)
+        // let name = this.props.name;
+        // let age = this.props.age;
+
+        // let {name, age, arrJobs} = this.props;
+        
+        let arrJobs = this.props.arrJobs;
+        
+        return (
+            // <React.Fragment>
+            
+            <>
+                <div className="job-lists">
+                    {
+                        arrJobs.map((item,index) => {
+                            return (
+                                <div key={item.id}>
+                                    ID: {item.id} - Title: {item.title} - Salary: {item.salary}
+                                </div>
+                            )
+                        })
+                    }
+                </div>
+                
+            </>
+            // </React.Fragment>
+        )
+    }
+}
+ 
+export default ChildComponent
