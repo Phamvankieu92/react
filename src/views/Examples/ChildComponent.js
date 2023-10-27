b039d425c9a711898e0934e062433ebf822e6d56
Pham Van Kieu <phamvankieu92@gmail.com>
2023-10-27 11:44:21 +0700

    SCSS

--- src/views/Examples/ChildComponent.js
+++ src/views/Examples/ChildComponent.js
@@ -1,4 +1,5 @@
 import React from "react";
+import './Demo.scss'
 
 class ChildComponent extends React.Component {
     state = {
@@ -30,7 +31,7 @@ class ChildComponent extends React.Component {
             
             <>
                 {showJob === false ?
-                    <div><button onClick={()=>this.handleShowHide()} >Show</button></div>
+                    <div><button onClick={()=>this.handleShowHide()} className="btn-show" >Show</button></div>
                 :
                 <>
                     <div className="job-lists">
@@ -51,7 +52,7 @@ class ChildComponent extends React.Component {
                             })
                         }
                     </div>
-                    <div><button onClick={()=>this.handleShowHide()}>Hide</button></div>
+                    <div><button className="btn-show" onClick={()=>this.handleShowHide()}>Hide</button></div>
                 </>
                 }
             </>
